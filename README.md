6f612292a060e1c4f07911c74f2d6b7619b81b58
Avery Magnotti <averymagnotti@outlook.com>
2016-11-12 17:39:38 -0500

    ａｅｓｔｈｅｔｉｃ

---
+++ README.md
@@ -0,0 +1,7 @@
+# [ｆｕｌｌ　ｗｉｄｔｈ](https://width.citrusui.me)
+
+Full width character generator. Improved from [this site](http://wwwwwyoutube.com/fullw.html).
+
+Licensed under [MIT](LICENSE.md).
+
+Disclaimer: @bsdf [gave me the OK](https://twitter.com/bsdf/status/797484933354815488).
---
+++ width.js
@@ -0,0 +1,26 @@
+var $ = function (what) { return document.getElementById(what); };
+
+function handle(what) {
+  var t = fullw(what.value);
+  $("dis").innerHTML = t.replace(/\n/g, "\n<br>\n");
+}
+
+function fullw(str) {
+    var code, uchr, ustr;
+    var out = "";
+    for (var i=0; i<str.length; i++) {
+      code = str.charCodeAt(i);
+      if (code > 31 && code < 127) {
+        if (code == 32) {
+          uchr = 0x3000;
+        } else {
+          uchr = 0xFEE0 + code;
+        }
+        ustr = "" + eval("'\\u" + uchr.toString(16) + "'");
+        out += ustr;
+      } else {
+        out += str.charAt(i);
+      }
+    }
+    return out;
+  }
